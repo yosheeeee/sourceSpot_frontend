a56cfa68e92d60b989741da7e17c980a0ed8a170
Antipin Egor <evantipin@edu.hse.ru>
2025-03-18 21:53:06 +0500

    add static data

--- src/models/types.ts
+++ src/models/types.ts
@@ -1,13 +1,22 @@
 export interface IAuthor {
-	nickname: string;
-	avatarSrc?: string;
+  nickname: string;
+  avatarSrc?: string;
+}
+
+export interface IComment {
+  author: IAuthor;
+  content: string;
 }
 
 export interface IProjectProps {
-	id: number;
-	name: string;
-	imageSrc?: string;
-	description?: string;
-	author: IAuthor;
-	tags: string[];
+  id: number;
+  name: string;
+  imageSrc?: string;
+  description?: string;
+  author: IAuthor;
+  tags: string[];
+  comments?: IComment[];
+  gitHubUrl?: string;
+  likes: number;
+  views: number;
 }
